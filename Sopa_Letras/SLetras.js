dc7d5941b7982d31d351f790753985f5d15a5f81
Richard Buitrago <99455263+Richardevelop@users.noreply.github.com>
2022-05-31 15:20:52 -0500

    Update SLetras.js

--- Sopa_Letras/SLetras.js
+++ Sopa_Letras/SLetras.js
@@ -1,126 +1,413 @@
-var a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,
-	b10,c6,c11, d6,e1,e2,e3,e4,e5,e6,e7,e8,e9,e10,e11,
-	f6,g6,h6,i3,i4,i5,i6,i7,i8,i9,j6;
-
-//palabra JavaScript color rojo
-function comprobar1(){
-	a1 = document.getElementById('celda1').style.color="red";
+function JAVASCRIPT(celda){
+	celda.style.backgroundColor = "#cdb4db";
 }
 
-/* function comprobar2(){
-	a2 = document.getElementById('celda2').style.color="red";
-} */
-function comprobar3(){
-	a3 = document.getElementById('celda3').style.color="red";
+function PHP(celda){
+	celda.style.backgroundColor = "#dda15e";
 }
-function comprobar4(){
-	a4 = document.getElementById('celda4').style.color="red";
+function sombrear2(celda){
+	celda.style.backgroundColor = "#219ebc";
 }
 
-function comprobar5(){
-	a5 = document.getElementById('celda5').style.color="red";
+function sombrear3(celda){
+	celda.style.backgroundColor = "#76c893";
+}
+function sombrear4(celda){
+	celda.style.backgroundColor = "#011627";
+}
+function sombrear5(celda){
+	celda.style.backgroundColor = "#fff3b0";
+}
+function sombrear6(celda){
+	celda.style.backgroundColor = "#936639";
+}
+function sombrear7(celda){
+	celda.style.backgroundColor = "#8b8c89";
+}
+function sombrear8(celda){
+	celda.style.backgroundColor = "#48cae4";
+}
+function sombrear9(celda){
+	celda.style.backgroundColor = "#73fbd3";
+}
+function sombrear10(celda){
+	celda.style.backgroundColor = "#99d98c";
+}
+function sombrear11(celda){
+	celda.style.backgroundColor = "#bf0603";
+}
+function sombrear12(celda){
+	celda.style.backgroundColor = "yellow";
+}
+function sombrear13(celda){
+	celda.style.backgroundColor = "black";
+}
+function sombrear14(celda){
+	celda.style.backgroundColor = "green";
+}
+function WEB(celda){
+	celda.style.backgroundColor = "#fbc4ab";
+}
+function sombrear16(celda){
+	celda.style.backgroundColor = "purple";
+}
+function sombrear17(celda){
+	celda.style.backgroundColor = "#ffe3e0";
+}
+function sombrear18(celda){
+	celda.style.backgroundColor = "#ff7d00";
 }
 
-function comprobar6(){
-	a6 = document.getElementById('celda6').style.color="red";
+
+
+
+//palabra JAVASCRIPT 
+function JAVASCRIPT1(){
+	a1 = document.getElementById('celda1').style.color="white";	
 }
 
-function comprobar7(){
-	a7 = document.getElementById('celda7').style.color="red";
+function JAVASCRIPT2(){
+	a2= document.getElementById('celda2').style.color="white";
 }
-function comprobar8(){
-	a8 = document.getElementById('celda8').style.color="red";
+function JAVASCRIPT3(){
+	a3 = document.getElementById('celda3').style.color="white";
 }
-function comprobar9(){
-	a9 = document.getElementById('celda9').style.color="red";
-}
-function comprobar10(){
-	a10 = document.getElementById('celda10').style.color="red";
-}
-//palabra php color moprado
-function comprobarB10(){
-	b10 = document.getElementById('celdaB10').style.color="blue";
+function JAVASCRIPT4(){
+	a4 = document.getElementById('celda4').style.color="white";
 }
 
-function comprobarC11(){
-	c11 = document.getElementById('celdaC11').style.color="blue";
-}
-//palabra lenguaje color cafe
-function comprobarC6(){
-	c6 = document.getElementById('celdaC6').style.color="brown";
-}
-function comprobarD6(){
-	d6 = document.getElementById('celdaD6').style.color="brown";
+function JAVASCRIPT5(){
+	a5 = document.getElementById('celda5').style.color="white";
 }
 
-function comprobarE6(){
-	e6 = document.getElementById('celdaE6').style.color="brown";
+function JAVASCRIPT6(){
+	a6 = document.getElementById('celda6').style.color="white";
 }
 
-function comprobarF6(){
-	f6 = document.getElementById('celdaF6').style.color="brown";
+function JAVASCRIPT7(){
+	a7 = document.getElementById('celda7').style.color="white";
 }
-function comprobarG6(){
-	g6 = document.getElementById('celdaG6').style.color="brown";
+function JAVASCRIPT8(){
+	a8 = document.getElementById('celda8').style.color="white";
 }
-
-function comprobarH6(){
-	h6 = document.getElementById('celdaH6').style.color="brown";
+function JAVASCRIPT9(){
+	a9 = document.getElementById('celda9').style.color="white";
 }
-function comprobarI6(){
-	i6 = document.getElementById('celdaI6').style.color="brown";
-}
-function comprobarJ6(){
-	j6 = document.getElementById('celdaJ6').style.color="brown";
+function JAVASCRIPT0(){
+	a0 = document.getElementById('celda0').style.color="white";
 }
 
-//palabra laboratoria
-function comprobarE1(){
-	e1 = document.getElementById('celdaE1').style.color="yellow";
+//palabra PHP
+function PHP10(){
+	a10 = document.getElementById('celda10').style.color="yellow";
 }
-function comprobarE2(){
-	e2 = document.getElementById('celdaE2').style.color="yellow";
+function PHP11(){
+	a11 = document.getElementById('celda11').style.color="yellow";
 }
-function comprobarE3(){
-	e3 = document.getElementById('celdaE3').style.color="yellow";
+
+// Lenguaje
+
+function comprobar12(){
+	e12 = document.getElementById('celda12').style.color="#ffb4a2";
 }
-function comprobarE4(){
-	e4 = document.getElementById('celdaE4').style.color="yellow";
+function comprobar13(){
+	e13 = document.getElementById('celda13').style.color="#ffb4a2";
 }
-function comprobarE5(){
-	e5 = document.getElementById('celdaE5').style.color="yellow";
+function comprobar14(){
+	e14 = document.getElementById('celda14').style.color="#ffb4a2";
 }
-function comprobarE7(){
-	e7 = document.getElementById('celdaE7').style.color="yellow";
+function comprobar15(){
+	e15 = document.getElementById('celda15').style.color="#ffb4a2";
 }
-function comprobarE8(){
-	e8 = document.getElementById('celdaE8').style.color="yellow";
+function comprobar16(){
+	e16 = document.getElementById('celda16').style.color="#ffb4a2";
 }
-function comprobarE9(){
-	e9 = document.getElementById('celdaE9').style.color="yellow";
+function comprobar17(){
+	e17 = document.getElementById('celda17').style.color="#ffb4a2";
 }
-function comprobarE10(){
-	e10 = document.getElementById('celdaE10').style.color="yellow";
+function comprobar18(){
+	e18 = document.getElementById('celda18').style.color="#ffb4a2";
 }
-function comprobarE11(){
-	e11 = document.getElementById('celdaE11').style.color="yellow";
+function comprobar19(){
+	e19 = document.getElementById('celda19').style.color="#ffb4a2";
 }
-//palabra aprendo color orange
-function comprobarI3(){
-	i3 = document.getElementById('celdaI3').style.color="orange";
+
+
+// GIT
+function comprobar20(){
+	e20 = document.getElementById('celda20').style.color="#e85d04";
 }
-function comprobarI4(){
-	i4 = document.getElementById('celdaI4').style.color="orange";
+function comprobar21(){
+	e21 = document.getElementById('celda21').style.color="#e85d04";
 }
-function comprobarI5(){
-	i5 = document.getElementById('celdaI5').style.color="orange";
+function comprobar22(){
+	e22 = document.getElementById('celda22').style.color="#e85d04";
 }
-function comprobarI7(){
-	i7 = document.getElementById('celdaI7').style.color="orange";
+
+// GITHUB
+
+function comprobar23(){
+	e23 = document.getElementById('celda23').style.color="#00bbf9";
 }
-function comprobarI8(){
-	i8 = document.getElementById('celdaI8').style.color="orange";
+function comprobar24(){
+	e24 = document.getElementById('celda24').style.color="#00bbf9";
 }
-function comprobarI9(){
-	i9 = document.getElementById('celdaI9').style.color="orange";
+function comprobar25(){
+	e25 = document.getElementById('celda25').style.color="#00bbf9";
+}
+function comprobar26(){
+	e26 = document.getElementById('celda26').style.color="#00bbf9";
+}
+function comprobar27(){
+	e27 = document.getElementById('celda27').style.color="#00bbf9";
+}
+function comprobar28(){
+	e28 = document.getElementById('celda28').style.color="#00bbf9";
+}
+
+// FIGMA
+
+function comprobar29(){
+	e29 = document.getElementById('celda29').style.color="#00f5d4";
+}
+function comprobar30(){
+	e30 = document.getElementById('celda30').style.color="#00f5d4";
+}
+function comprobar31(){
+	e31 = document.getElementById('celda31').style.color="#00f5d4";
+}
+function comprobar32(){
+	e32 = document.getElementById('celda32').style.color="#00f5d4";
+}
+function comprobar33(){
+	e33 = document.getElementById('celda33').style.color="#00f5d4";
+}
+
+// NEOCITIES
+function comprobar34(){
+	e34 = document.getElementById('celda34').style.color="white";
+}
+function comprobar35(){
+	e35 = document.getElementById('celda35').style.color="white";
+}
+function comprobar36(){
+	e36 = document.getElementById('celda36').style.color="white";
+}
+function comprobar37(){
+	e37 = document.getElementById('celda37').style.color="white";
+}
+function comprobar38(){
+	e38 = document.getElementById('celda38').style.color="white";
+}
+function comprobar39(){
+	e39 = document.getElementById('celda39').style.color="white";
+}
+function comprobar40(){
+	e40 = document.getElementById('celda40').style.color="white";
+}
+function comprobar41(){
+	e41= document.getElementById('celda41').style.color="white";
+}
+function comprobar42(){
+	e42 = document.getElementById('celda42').style.color="white";
+}
+
+// HTTP
+function comprobar43(){
+	e43 = document.getElementById('celda43').style.color="#ff6d00";
+}
+function comprobar44(){
+	e44 = document.getElementById('celda44').style.color="#ff6d00";
+}
+function comprobar45(){
+	e45 = document.getElementById('celda45').style.color="#ff6d00";
+}
+function comprobar46(){
+	e46 = document.getElementById('celda46').style.color="#ff6d00";
+}
+
+// TCP
+function comprobar47t(){
+	e47t = document.getElementById('celda47t').style.color="#ff6d00";
+}
+function comprobar47(){
+	e47 = document.getElementById('celda47').style.color="#ff6d00";
+}
+function comprobar48(){
+	e48 = document.getElementById('celda48').style.color="#ff6d00";
+}
+
+// DNS
+
+function comprobar49(){
+	e49 = document.getElementById('celda49').style.color="#e70e02";
+}
+function comprobar50(){
+	e50 = document.getElementById('celda50').style.color="#e70e02";
+}
+function comprobar51(){
+	e51 = document.getElementById('celda51').style.color="#e70e02";
+}
+
+// WWWW
+function comprobar52(){
+	e52 = document.getElementById('celda52').style.color="#ffca3a";
+}
+function comprobar53(){
+	e53 = document.getElementById('celda53').style.color="#ffca3a";
+}
+function comprobar54(){
+	e54 = document.getElementById('celda54').style.color="#ffca3a";
+}
+function comprobar54(){
+	e54 = document.getElementById('celda54').style.color="#ffca3a";
+}
+
+// FTP
+function comprobar55(){
+	e55 = document.getElementById('celda55').style.color="#ffca3a";
+}
+function comprobar56(){
+	e56 = document.getElementById('celda56').style.color="#ffca3a";
+}
+function comprobar57(){
+	e57 = document.getElementById('celda57').style.color="#ffca3a";
+}
+
+// HTML
+function comprobar58(){
+	e58 = document.getElementById('celda58').style.color="#560bad";
+}
+function comprobar59(){
+	e59 = document.getElementById('celda59').style.color="#560bad";
+}
+function comprobar60(){
+	e60 = document.getElementById('celda60').style.color="#560bad";
+}
+function comprobar61(){
+	e61 = document.getElementById('celda61').style.color="#560bad";
+}
+
+// CSS
+function comprobar62(){
+	e62 = document.getElementById('celda62').style.color="yellow";
+}
+function comprobar63(){
+	e63 = document.getElementById('celda63').style.color="yellow";
+}
+function comprobar64(){
+	e64 = document.getElementById('celda64').style.color="yellow";
+}
+
+// ETIQUETA
+
+function comprobar65(){
+	e65 = document.getElementById('celda65').style.color="#e6beae";
+}
+function comprobar66(){
+	e66 = document.getElementById('celda66').style.color="#e6beae";
+}
+function comprobar67(){
+	e67 = document.getElementById('celda67').style.color="#e6beae";
+}
+function comprobar68(){
+	e68 = document.getElementById('celda68').style.color="#e6beae";
+}
+function comprobar69(){
+	e69 = document.getElementById('celda69').style.color="#e6beae";
+}
+function comprobar70(){
+	e70 = document.getElementById('celda70').style.color="#e6beae";
+}
+function comprobar71(){
+	e71 = document.getElementById('celda71').style.color="#e6beae";
+}
+function comprobar72(){
+	e72 = document.getElementById('celda72').style.color="#e6beae";
+}
+
+// WEB
+
+function WEB73(){
+	e73 = document.getElementById('celda73').style.color="#582f0e";
+}
+
+function WEB74(){
+	e74 = document.getElementById('celda74').style.color="#582f0e";
+}
+function WEB75(){
+	e75 = document.getElementById('celda75').style.color="#582f0e";
+}
+
+// PROGRAMACION
+
+function comprobar76(){
+	e76 = document.getElementById('celda76').style.color="#ffea00";
+}
+function comprobar77(){
+	e77 = document.getElementById('celda77').style.color="#ffea00";
+}
+function comprobar78(){
+	e78 = document.getElementById('celda78').style.color="#ffea00";
+}
+function comprobar79(){
+	e79 = document.getElementById('celda79').style.color="#ffea00";
+}
+function comprobar80(){
+	e80 = document.getElementById('celda80').style.color="#ffea00";
+}
+function comprobar81(){
+	e81 = document.getElementById('celda81').style.color="#ffea00";
+}
+function comprobar82(){
+	e82 = document.getElementById('celda82').style.color="#ffea00";
+}
+function comprobar83(){
+	e83 = document.getElementById('celda83').style.color="#ffea00";
+}
+function comprobar84(){
+	e84= document.getElementById('celda84').style.color="#ffea00";
+}
+function comprobar85(){
+	e85 = document.getElementById('celda85').style.color="#ffea00";
+}
+function comprobar86(){
+	e86 = document.getElementById('celda86').style.color="#ffea00";
+}
+function comprobar87(){
+	e87 = document.getElementById('celda87').style.color="#ffea00";
+}
+
+// SERVIDOR
+function comprobar88(){
+	e88 = document.getElementById('celda88').style.color="#1b2021";
+}
+function comprobar89(){
+	e89 = document.getElementById('celda89').style.color="#1b2021";
+}
+function comprobar90(){
+	e90 = document.getElementById('celda90').style.color="#1b2021";
+}
+function comprobar91(){
+	e91 = document.getElementById('celda91').style.color="#1b2021";
+}
+function comprobar92(){
+	e92= document.getElementById('celda92').style.color="#1b2021";
+}
+function comprobar93(){
+	e93 = document.getElementById('celda93').style.color="#1b2021";
+}
+function comprobar94(){
+	e94 = document.getElementById('celda94').style.color="#1b2021";
+}
+function comprobar95(){
+	e95 = document.getElementById('celda95').style.color="#1b2021";
+}
+
+// IDE
+function comprobar96(){
+	e96 = document.getElementById('celda96').style.color="#80ffdb";
+}
+function comprobar97(){
+	e97 = document.getElementById('celda97').style.color="#80ffdb";
 }
